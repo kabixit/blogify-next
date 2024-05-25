8c2d2519ae525dacdf16bed9ad984cbc3badeaa9
KumaraKabilan <kumarakabilan2004@gmail.com>
2024-05-26 02:16:07 +0530

    Update Login.tsx

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -25,7 +25,7 @@ const Login = () => {
   };
 
   const navigateToSignUp = () => {
-    router.push('/SignUp'); // Navigate to the signu
+    router.push('/SignUp'); // Navigate to the sign
   return (
     <div>
       <Box
